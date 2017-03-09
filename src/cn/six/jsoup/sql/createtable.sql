81371e30c8e233e7dd5bcef90e6a50442bb8c9fe
wangyoushun <2436093367@qq.com>
2017-03-09 17:03:53 +0800

    1

---
+++ src/cn/six/jsoup/sql/createtable.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL COMMENT '职位标题',
+  `company_name` varchar(30) DEFAULT NULL COMMENT '公司名称',
+  `industry` varchar(50) DEFAULT NULL COMMENT '介绍',
+  `money_left` bigint(20) DEFAULT NULL COMMENT '最低工资',
+  `money_right` bigint(20) DEFAULT NULL COMMENT '最高工资',
+  `create_time` varchar(20) DEFAULT NULL COMMENT '发布时间',
+  `input_time` datetime DEFAULT NULL,
+  `area` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+--------------------------------------
+
+CREATE TABLE `job_detail` (
+  `id` int(11) NOT NULL,
+  `job_id` int(11) DEFAULT NULL,
+  `job_advantage` varchar(100) DEFAULT NULL COMMENT '工作优势',
+  `description` text COMMENT '描述',
+  `address` varchar(50) DEFAULT NULL COMMENT '地址',
+  `hr_name` varchar(10) DEFAULT NULL COMMENT 'hr名字',
+  `url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
