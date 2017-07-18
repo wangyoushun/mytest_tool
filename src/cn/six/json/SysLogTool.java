935f6fa2d0fcdb8d423cbffe8329cc6a8d39d341
iwantfly <2436093367@qq.com>
2017-07-18 22:26:32 +0800

    add

--- src/cn/six/json/SysLogTool.java
+++ src/cn/six/json/SysLogTool.java
@@ -1,5 +1,13 @@
 package cn.six.json;
 
+/**
+ * 
+* @ClassName: SysLogTool 
+* @Description: 封装system输出日志
+* @author iwantfly
+* @date 2017年7月16日 下午11:06:22 
+*
+ */
 public class SysLogTool {
 	private boolean consolelog=false;
 	
